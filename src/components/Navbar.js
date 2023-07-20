b15ceee021639664649e0ad042e1d1f6058d7cd6
Moosa Raheel <moosarahil13@gmail.com>
2023-07-20 07:11:07 +0500

    Improve responsive dropdown navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,73 @@
 import React, { Component } from "react";
 import logo from "../img/logo.png";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-  static defaultProps = {
-    one: "Home",
-    two: "Categories",
-    three: "About",
-    four: "Contact",
-  };
-  static propTypes = {
-    one: PropTypes.string,
-    two: PropTypes.string,
-    three: PropTypes.string,
-    four: PropTypes.string,
-  };
-  toggle() {
-    const header = document.getElementsByClassName("header")[0];
+  handleNav() {
+    const header = document.getElementsByTagName("header")[0];
     header.classList.toggle("active");
-  }
-  working() {
-    alert("We Still Working On It");
+    const li = document.querySelectorAll("ul li");
+    for (const element of li) {
+      if (!element.classList.contains("dis")) {
+        element.addEventListener("click", () => {
+          header.classList.remove("active");
+          console.log("click");
+        });
+      }
+    }
   }
   render() {
     return (
       <>
-        <header className="header ">
+        <header>
           <div className="logo">
-            <img src={logo} alt="" />
+            <img src={logo} alt="Logo" />
           </div>
-          <nav className="navbar">
-            <ul className="nav-items">
-              <li className="nav-item">
-                <Link to="/">{this.props.one}</Link>
-              </li>
-              <li className="nav-item">
-                {this.props.two}
-                <ul className="dropdown">
-                  <li>
-                    <Link to="/entertainment">Entertainment</Link>
-                  </li>
-                  <li>
-                    <Link to="/health">Health</Link>
-                  </li>
-                  <li>
-                    <Link to="/tech">Tech</Link>
-                  </li>
-                  <li>
-                    <Link to="/sports">Sports</Link>
-                  </li>
-                  <li>
-                    <Link to="/business">Business</Link>
-                  </li>
-                  <li>
-                    <Link to="/politics">Politics</Link>
-                  </li>
-                  <li>
-                    <Link to="/science">Science</Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item" onClick={this.working}>
-                {this.props.three}
-              </li>
-              <li className="nav-item" onClick={this.working}>
-                {this.props.four}
-              </li>
-            </ul>
-          </nav>
-          <div className="mobile-navbar-btn" onClick={this.toggle}>
+          <div className="icons" onClick={this.handleNav}>
             <i className="fa-solid fa-bars open"></i>
             <i className="fa-solid fa-xmark close"></i>
           </div>
+          <nav className="navbar">
+            <ul>
+              <li className="nav-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="nav-item dis">
+                <Link to="#">
+                  Catagories <span>+</span>
+                </Link>
+                <ul>
+                  <li>
+                    <Link to="/entertainment">entertainment</Link>
+                  </li>
+                  <li>
+                    <Link to="/business">business</Link>
+                  </li>
+                  <li>
+                    <Link to="/sports">sports</Link>
+                  </li>
+                  <li>
+                    <Link to="/business">business</Link>
+                  </li>
+                  <li>
+                    <Link to="/science">science</Link>
+                  </li>
+                  <li>
+                    <Link to="/technology">technology</Link>
+                  </li>
+                  <li>
+                    <Link to="/health">health</Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
         </header>
       </>
     );
